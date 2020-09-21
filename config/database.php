51c81811ad94f2f391a35dd86999ac9331186354
prodigalson <prodigalson@localhost.localdomain>
2020-09-21 11:16:50 -0400

    db config

--- config/database.php
+++ config/database.php
@@ -73,15 +73,16 @@ return [
         ],
  */
 
- /* 'mysql' => [
+  'mysql' => [
     'driver' => 'mysql',
-    'url' => env('DATABASE_URL'),
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'forge'),
-    'username' => env('DB_USERNAME', 'forge'),
-    'password' => env('DB_PASSWORD', ''),
-    'unix_socket' => env('DB_SOCKET', ''),
+   // 'url' => env('DATABASE_URL'),
+
+    //'port' => env('DB_PORT', '3306'),
+    'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+   // 'unix_socket' => env('DB_SOCKET', ''),
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
@@ -91,7 +92,7 @@ return [
     'options' => extension_loaded('pdo_mysql') ? array_filter([
         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
     ]) : [],
-], */
+],
 
         'pgsql' => [
             'driver' => 'pgsql',
@@ -120,7 +121,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
+/*
         'mysql
         ' => array(
             'driver' => 'mysql',
@@ -131,7 +132,7 @@ return [
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-        ),
+        ), */
 
 
 
